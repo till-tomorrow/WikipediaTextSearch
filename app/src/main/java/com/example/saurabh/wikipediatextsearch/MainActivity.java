7794f29d474a5d84dd1b32dfb0baad6914824ffa
Palak Jain <palakjain444@gmail.com>
2017-10-13 23:40:58 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/saurabh/wikipediatextsearch/MainActivity.java
+++ app/src/main/java/com/example/saurabh/wikipediatextsearch/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.saurabh.wikipediatextsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnClickMe;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+         btnClickMe = (Button) findViewById(R.id.btnClickMe);
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Toast.makeText(MainActivity.this, "Button Clicked!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
